f926f2b1c5127b568f452762528e579d94e164d6
smthubakgale <97933623+smthubakgale@users.noreply.github.com>
2024-10-24 10:40:42 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,46 @@
+"""
+Author : Thubakgale Mabalane (Sam)
+Description: A simple program to generate a fibonacci sequence by taking user input about the length
+"""
+
+import os
+
+def clear_screen():
+    if os.name == "nt":
+        os.system("cls")
+    else:
+        os.system("clear")
+        
 def fibo(num):
-    if num <= 1:
-        return 1
-    return fibo(num - 1) + fibo(num - 2)
+  if num <= 1:
+      return 1
+  return fibo(num - 1) + fibo(num - 2)
 
 def fibonacci(num):
-    arr = []
-    for i in range(num):
-        arr.append(fibo(i))
-    return arr
+  arr = []
+  for i in range(num):
+      arr.append(fibo(i))
+  return arr
 
-seq = fibonacci(6)
+if __name__ == "__main__":
+    print("Hex software Fibonacci Sequence Generator")
+    print("Welcome , press Enter to Continue")
 
-print(seq)
+    input()
+    clear_screen()
 
+    cn = True
+    while cn:
+        print("Enter length of sequence : ")
+        str = input()
+        
+        try:
+            num = int(str)
+
+            seq = fibonacci(num)
+            print(seq)
+
+            input()
+            clear_screen()
+        except:
+            print("incorrect input")
